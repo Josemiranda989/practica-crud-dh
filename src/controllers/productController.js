0bdf22330a74578c96036c5822f0fa3b183df6ae
unknown <leandromanuel291295@gmail.com>
2022-09-21 12:58:42 -0300

    agregando delete y multer

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,65 +1,66 @@
 const fs = require("fs");
 const path = require("path");
 
-function findAll(){
-   const jsonData = fs.readFileSync(path.join(__dirname,"../data/products.json"));
-   const data = JSON.parse(jsonData)
-   return data
+function findAll() {
+  const jsonData = fs.readFileSync(
+    path.join(__dirname, "../data/products.json")
+  );
+  const data = JSON.parse(jsonData);
+  return data;
 }
 
-function writeFile(data){
-    const dataString = JSON.stringify(data, null, 4)
-    fs.writeFileSync(path.join(__dirname, "../data/products.json"), dataString);
- 
+function writeFile(data) {
+  const dataString = JSON.stringify(data, null, 4);
+  fs.writeFileSync(path.join(__dirname, "../data/products.json"), dataString);
 }
 
 const controllers = {
-    list: (req,res)=>{
-        const data = findAll()
-        res.render("menu-products", { products:data })
-    },
-    detail: (req,res)=>{
-        const data = findAll()
-        const platoEncontrado = data.find(function(plato){
-            return plato.id == req.params.id
-        })
-        res.render("product-detail", { plato:platoEncontrado })
-    },
-    create:(req,res)=>{
-        res.render("product-create-form")
-    },
-    store:(req,res)=>{
-        const data = findAll()
-        
-        const newProduct = {
-            id: data.length + 5,
-            name: req.body.name,
-            price: req.body.price,
-            description: req.body.description
-        }
+  list: (req, res) => {
+    const data = findAll();
+    res.render("menu-products", { products: data });
+  },
+  detail: (req, res) => {
+    const data = findAll();
+    const platoEncontrado = data.find(function (plato) {
+      return plato.id == req.params.id;
+    });
+    res.render("product-detail", { plato: platoEncontrado });
+  },
+  create: (req, res) => {
+    res.render("product-create-form");
+  },
+  store: (req, res) => {
+    const data = findAll();
+    console.log(req.file);
 
-        data.push(newProduct);
+    const newProduct = {
+      id: data.length + 5,
+      name: req.body.name,
+      price: req.body.price,
+      description: req.body.description,
+      image: req.file
+    };
 
-        writeFile(data);
+    data.push(newProduct);
 
-        res.redirect("/products/list");
-    },
-    edit:(req,res)=>{
-        const data = findAll()
+    writeFile(data);
 
-        const platoEncontrado = data.find(function(plato){
-            return plato.id == req.params.id
+    res.redirect("/products/create");
+  },
+  edit: (req, res) => {
+    const data = findAll();
 
-        })
-        res.render("product-update-form",{ plato:platoEncontrado });
-},
-update:(req,res)=>{
-    const data = findAll()
+    const platoEncontrado = data.find(function (plato) {
+      return plato.id == req.params.id;
+    });
+    res.render("product-update-form", { plato: platoEncontrado });
+  },
+  update: (req, res) => {
+    const data = findAll();
 
-    const platoEncontrado = data.find(function(plato){
-        return plato.id == req.params.id
-
-    })
+    const platoEncontrado = data.find(function (plato) {
+      return plato.id == req.params.id;
+    });
 
     platoEncontrado.name = req.body.name;
     platoEncontrado.price = req.body.price;
@@ -67,8 +68,21 @@ update:(req,res)=>{
 
     writeFile(data);
 
-    res.redirect("/products/list")
-}
-}
+    res.redirect("/products/list");
+  },
+  destroy: (req, res) => {
+    const data = findAll();
+
+    const platoEncontrado = data.findIndex(function (plato) {
+      return plato.id == req.params.id;
+    });
+
+    data.splice(platoEncontrado, 1);
+
+    writeFile(data);
+
+    res.redirect("/products/list");
+  },
+};
 
 module.exports = controllers;
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const router = express.Router()
 const productController = require("../controllers/productController")
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        cb(null, path.join(__dirname,"../../public/img/imagenes-platos"))
+    },
+    filename:(req, file, cb) => {
+        const newFileName = file.filename + Data.now() + "-" + path.extname(file.originalname)
+        cb(null, newFileName)
+    }
+});
+
+const upload = ({ storage: storage });
 
 router.get("/list", productController.list);
 router.get("/detail/:id", productController.detail);
 router.get("/create", productController.create);
-router.post("/create", productController.store);
+router.post("/create",upload.single("image") ,productController.store);
 router.get("/edit/:id", productController.edit);
 router.put("/edit/:id", productController.update);
+router.delete("/delete/:id", productController.destroy);
 
 
 module.exports = router
